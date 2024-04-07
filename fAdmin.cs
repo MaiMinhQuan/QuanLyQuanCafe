456295b5c6de2769bd1a803ce9f20e66f5049a37
MiQ <kokobay2004@gmail.com>
2024-04-08 00:16:20 +0700

    patern singleton

--- fAdmin.cs
+++ fAdmin.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using QuanLyQuanCafe.DAO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,11 +17,19 @@ namespace QuanLyQuanCafe
         public fAdmin()
         {
             InitializeComponent();
+            LoadAccountList();
         }
 
         private void label2_Click(object sender, EventArgs e)
         {
 
+        }
+
+        void LoadAccountList()
+        {
+            string query = "exec USP_GetAccountByUserName @username";
+
+            dtgvAccount.DataSource = DataProvider.Instance.ExecuteQuery(query, new object[] { "staff" });
         }
     }
 }
